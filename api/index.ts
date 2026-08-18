abc3f7d502c2212e86c71e113434dbb1d42ced5e
rensiyu6162-star <rensiyu6162@gmail.com>
2026-08-18 23:57:36 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,16 @@
+import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import app from "../src/app";
+import { cors } from "hono/cors";
+import { prettyJSON } from "hono/pretty-json";
+import registry from "../src/registry";
 
-export const runtime = "nodejs";
+const app = new Hono().basePath("/");
 
+app.use("*", cors({ origin: "*" }));
+app.use(prettyJSON());
+app.route("/", registry);
+
+app.get("/", (c) => c.json({ message: "DailyHot API is running on Vercel" }));
+
+export const config = { runtime: "edge" };
 export default handle(app);
