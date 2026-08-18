dba59b1e0272607fa164a3c0490411990c0d427b
rensiyu6162-star <rensiyu6162@gmail.com>
2026-08-18 23:46:33 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,6 @@
 import { handle } from "hono/vercel";
-import app from "../src/app.js";
+import app from "../src/app";
 
-export const config = {
-  runtime: "nodejs",
-};
+export const runtime = "nodejs";
 
 export default handle(app);
